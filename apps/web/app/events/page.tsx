d1308138efbc58fed24272e18674f65bd465c406
daw2-miguelangelsaiz26 <saizangullomiguelangel@fpllefia.com>
2026-06-02 20:32:25 +0200

    D3 — Missatge d'error

--- apps/web/app/events/page.tsx
+++ apps/web/app/events/page.tsx
@@ -31,6 +31,7 @@ export default function events() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       {loading && <p>Cargando los datos de la api...</p>}
+      {error && <p>Se ha detectado un error: {error}</p>}
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         {eventos.map((evento) => (
           <EventCard key={evento.id} evento={evento} />
